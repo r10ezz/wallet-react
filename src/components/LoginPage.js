bfa09afcbaafa54972e2e4582a7f8fcf1d156e57
Raj Singh <knotr10@gmail.com>
2024-10-23 07:33:49 +0000

    Fixed Login Page Button Issue

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,101 +1,12 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import Form from './Form';
 
-const Form = ({ buttonName }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [repeatPassword, setRepeatPassword] = useState('');
-    const [errors, setErrors] = useState({});
-
-    const validate = () => {
-        const newErrors = {};
-        
-        if (!email) {
-            newErrors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            newErrors.email = 'Email is invalid';
-        }
-        
-        if (!password) {
-            newErrors.password = 'Password is required';
-        } else if (password.length < 6) {
-            newErrors.password = 'Password must be at least 6 characters';
-        }
-        
-        if (!repeatPassword) {
-            newErrors.repeatPassword = 'Please repeat your password';
-        } else if (repeatPassword !== password) {
-            newErrors.repeatPassword = 'Passwords do not match';
-        }
-
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (validate()) {
-            console.log('Form submitted:', { email, password });
-        }
-    };
-
-    return (
-        <div className="form-wrapper">
-            <form className="form-container" onSubmit={handleSubmit} noValidate>
-                <div className="input-group">
-                    <label htmlFor="email2">Your email</label>
-                    <input
-                        id="email2"
-                        type="email"
-                        placeholder="name@domain.com"
-                        required
-                        className="input-field"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    {errors.email && <span className='error'>{errors.email}</span>}
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password2">Your password</label>
-                    <input
-                        id="password2"
-                        type="password"
-                        required
-                        className="input-field"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {errors.password && <span className='error'>{errors.password}</span>}
-                </div>
-                <div className="input-group">
-                    <label htmlFor="repeat-password">Repeat password</label>
-                    <input
-                        id="repeat-password"
-                        type="password"
-                        required
-                        className="input-field"
-                        value={repeatPassword}
-                        onChange={(e) => setRepeatPassword(e.target.value)}
-                    />
-                    {errors.repeatPassword && <span className='error'>{errors.repeatPassword}</span>}
-                </div>
-                <div className="checkbox-group">
-                    <input type="checkbox" id="agree" required />
-                    <label htmlFor="agree">
-                        I agree with the&nbsp;
-                        <a href="#" className="terms-link">
-                            terms and conditions
-                        </a>
-                    </label>
-                </div>
-                <button type="submit" className="submit-button">{buttonName}</button>
-                
-                <div className="login-prompt">
-                    <p>Do not have an account? <Link to="/signup" className="login-link">Sign Up</Link></p>
-                </div>
-            </form>
-        </div>
-    );
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <Form buttonName="Log In" />
+    </div>
+  );
 };
 
-export default Form;
+export default LoginPage;
